e954b13ad5921374b24de9d2d938d68529e29aa7
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-02-15 10:56:50 +0900

    [Bronze V] Title: 이상한 기호, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		long A = scanner.nextLong();
+		
+		long B = scanner.nextLong();
+		
+		long result = (A+B)*(A-B);
+		
+		System.out.println(result);
+	}
+}
