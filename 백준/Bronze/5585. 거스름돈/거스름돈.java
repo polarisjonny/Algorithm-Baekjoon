e268a096155f2951c0f0119001b930ab0429a697
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-07-16 20:38:47 +0900

    [Bronze II] Title: 거스름돈, Time: 208 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        
-        //거스름돈
-        int change = 1000 - scanner.nextInt();
-        
-        //동전의 갯수
-        int count = 0;
-        
-        //동전 배열
+
         int[] coins = {500, 100, 50, 10, 5, 1};
-        
+
+        int bill = 1000;
+
+        int payMoney = scanner.nextInt();
+
+        int change = bill - payMoney;
+
+        int count = 0;
+
         for(int i = 0; i < coins.length; i++) {
-            if(change % coins[i] >= 0) {
-                count += change/coins[i];
+
+                count += change / coins[i];
+
                 change = change % coins[i];
-            }
         }
         System.out.println(count);
     }
