56622972d27674b5fd95c8bb21545fada92eb46c
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-08-25 16:48:04 +0900

    [Silver V] Title: 뒤집기, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,22 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args)  throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      String input = br.readLine();
+
+      StringTokenizer st1 = new StringTokenizer(input, "0");
+
+      StringTokenizer st0 = new StringTokenizer(input, "1");
+
+      System.out.println(Math.min(st1.countTokens(), st0.countTokens()));
+    }
+}
