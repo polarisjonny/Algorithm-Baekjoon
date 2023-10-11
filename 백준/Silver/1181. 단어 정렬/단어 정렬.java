eca964c8f71330f8ec630361bcf404aa2efd1b87
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-10-11 16:18:01 +0900

    [Silver V] Title: 단어 정렬, Time: 572 ms, Memory: 22732 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,45 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Scanner;
+
+
+
+
+public class Main {
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int N = Integer.parseInt(br.readLine());
+
+      String[] words = new String[N];
+
+      for(int i = 0; i < N; i++) {
+         words[i] = br.readLine();
+      }
+
+      Arrays.sort(words, new Comparator<String>() {
+         @Override
+         public int compare(String o1, String o2) {
+            if(o1.length() == o2.length()) {
+               return o1.compareTo(o2);
+            }
+
+            else {
+               return o1.length() - o2.length();
+            }
+         }
+      });
+
+      System.out.println(words[0]);
+
+      for(int i = 1; i < N; i++) {
+         if(!words[i].equals(words[i-1])) {
+            System.out.println(words[i]);
+         }
+      }
+   }
+}
