8f8094945d65e32f2355f4c84e8eaeafaf95528f
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-10-24 14:23:45 +0900

    [level 2] Title: 올바른 괄호, Time: 18.87 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,34 +1,22 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-
-        int countA = 0;
         
-        int countB = 0;
-        
-        if(s.charAt(0) == ')') {
-            answer = false;
-            return answer;
-        }
+        Stack<Character> stack = new Stack<>();
         
         for(int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == '(') {
-                countA++;
+                stack.push('(');
             } else if(s.charAt(i) == ')') {
-                countB++;
-            }
-            
-            if(countB > countA) {
-                return false;
+                if(stack.isEmpty()) {
+                    return false;
+                } 
+                stack.pop();
             }
         }
         
-        if(countA == countB) {
-            answer = true;
-        } else {
-            answer = false;
-        }
-
-        return answer;
+        return stack.isEmpty();
     }
 }
