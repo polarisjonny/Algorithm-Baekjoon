9ce88aaa8d1216ca8a0a637195882654817e0ad0
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-10-25 14:17:44 +0900

    [level 2] Title: 짝지어 제거하기, Time: 55.05 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = -1;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()) {
+            if(!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        answer = stack.isEmpty() ? 1 : 0;
+        
+        return answer;
+    }
+}
