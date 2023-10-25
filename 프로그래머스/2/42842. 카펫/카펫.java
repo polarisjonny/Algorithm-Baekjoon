3bb63e92b9d54fa9eb1966ffb92e35e838ea1019
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-10-25 15:01:46 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int total = brown + yellow;
+        
+        for(int i = 1; i <= total; i++) {
+            int row = i;
+            int col = total / row;
+            
+            if(row > col) {
+                continue;
+            }
+            
+            if((row - 2) * (col - 2) == yellow) {
+                answer[0] = col;
+                answer[1] = row;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
