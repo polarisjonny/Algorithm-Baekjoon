0cee65c1f7116d771cf02c683abc4e49f690b653
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-12-11 16:59:26 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String temp = Integer.toString(n);
+        
+
+        
+        for(int i = 0; i < temp.length(); i++) { 
+            answer += temp.charAt(i) - '0';
+        }
+
+        return answer;
+    }
+}
