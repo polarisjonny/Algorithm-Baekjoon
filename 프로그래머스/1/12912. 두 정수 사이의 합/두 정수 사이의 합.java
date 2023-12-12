0a7322fadec835fb4366374bccda415dacd76319
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-12-12 17:33:22 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.09 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        
+        int temp = 0;
+        
+        if(a > b) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        
+        for(long i = a; i <= b; i++) {
+            answer += i;
+        }
+        
+        
+        return answer;
+    }
+}
