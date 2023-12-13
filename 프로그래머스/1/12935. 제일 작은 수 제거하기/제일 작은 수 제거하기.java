e76973b6f6ba935cd261348b79d1a98ec9a47f6e
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-12-13 18:16:35 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        
+        if(arr.length == 1) {
+            int[] answer = {-1};
+            return answer;
+        } 
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        
+        int[] answer = new int[arr.length - 1];
+        
+        int index = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] != min) {
+                answer[index++] = arr[i];
+            }
+        }
+        
+        
+        return answer;
+    }
+}
