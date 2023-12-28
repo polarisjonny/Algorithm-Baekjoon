373194144dfa1b29101067436035491009f1a124
polarisjonny <116703070+polarisjonny@users.noreply.github.com>
2023-12-28 18:13:59 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.08 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        
+        long pNum = Long.parseLong(p);
+        
+        for(int i = 0; i < t.length() - p.length() + 1; i++) {
+            long num = Long.parseLong(t.substring(i, i + p.length()));
+        
+            if(num <= pNum) answer++;
+        }
+        
+        return answer;
+        
+        
+    }
+}
